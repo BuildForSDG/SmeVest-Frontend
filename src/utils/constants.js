a37210dcd18e0f13fa129affa6bc3db98016cc53
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2020-05-21 17:31:26 +0100

    new

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const API_END_POINT = process.env.API_END_POINT || 'http://localhost:4000/api/v1';
+export const API_END_POINT = process.env.API_END_POINT || 'http://localhost:5000/api/v1';
 export const AUTH_TOKEN = 'AUTH_TOKEN';
